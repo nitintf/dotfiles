e3607b2d87981d18d4e2cf81e07f50242a5bf654
nitintf <nitinpanwar0802@gmail.com>
2024-04-29 19:24:08 +0530

    changed to soft theme for everforest

--- nvim/lua/nitintf/theme/init.lua
+++ nvim/lua/nitintf/theme/init.lua
@@ -1,5 +1,5 @@
 -- require("nitintf.theme.highlights").init()
 
-vim.cmd "let g:everforest_background = 'hard'"
+vim.cmd "let g:everforest_background = 'soft'"
 vim.cmd "let g:everforest_current_word = 'underline'"
 vim.cmd "colorscheme everforest"
