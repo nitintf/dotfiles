92f203a93b8566af8367e3a88ef1ecde8766cab9
nitintf <nitinpanwar0802@gmail.com>
2025-08-29 23:39:15 +0530

    added gitignore and very minimal nvim config for work

---
+++ home/dot_config/nvim/init.lua
@@ -0,0 +1,101 @@
+-- ========================================================================== --
+-- ==                           EDITOR SETTINGS                            == --
+-- ========================================================================== --
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 30
+
+-- ========================================================================== --
+-- ==                             KEYBINDINGS                              == --
+-- ========================================================================== --
+
+-- Space as leader key
+vim.g.mapleader = ' '
+
+-- Shortcuts
+vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '^')
+vim.keymap.set({'n', 'x', 'o'}, '<leader>l', 'g_')
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+
+-- Basic clipboard interaction
+vim.keymap.set({'n', 'x'}, 'gy', '"+y') -- copy
+vim.keymap.set({'n', 'x'}, 'gp', '"+p') -- paste
+
+-- Delete text
+vim.keymap.set({'n', 'x'}, 'x', '"_x')
+vim.keymap.set({'n', 'x'}, 'X', '"_d')
+
+-- Commands
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
+vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
+vim.keymap.set('n', '<leader><space>', '<cmd>buffers<cr>:buffer<Space>')
+vim.keymap.set('n', '<leader>e', '<cmd>Lexplore %:p:h<cr>')
+vim.keymap.set('n', '<leader>E', '<cmd>Lexplore<cr>')
+
+local function netrw_mapping()
+  local bufmap = function(lhs, rhs)
+    local opts = {buffer = true, remap = true}
+    vim.keymap.set('n', lhs, rhs, opts)
+  end
+
+  -- close window
+  bufmap('<leader>e', ':Lexplore<cr>')
+  bufmap('<leader>E', ':Lexplore<cr>')
+
+  -- Go back in history
+  bufmap('H', 'u')
+
+  -- Go up a directory
+  bufmap('h', '-^')
+
+  -- Open file/directory
+  bufmap('l', '<cr>')
+
+  -- Open file/directory then close explorer
+  bufmap('L', '<cr>:Lexplore<CR>')
+
+  -- Toggle dotfiles
+  bufmap('.', 'gh')
+end
+
+
+-- ========================================================================== --
+-- ==                               COMMANDS                               == --
+-- ========================================================================== --
+
+vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
+
+local group = vim.api.nvim_create_augroup('user_cmds', {clear = true})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = group,
+  desc = 'Highlight on yank',
+  callback = function()
+    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'help', 'man'},
+  group = group,
+  command = 'nnoremap <buffer> q <cmd>quit<cr>'
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'netrw',
+  group = group,
+  desc = 'Keybindings for netrw',
+  callback = netrw_mapping
+})
